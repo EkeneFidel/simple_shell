bdc89906949cd9738e1de944bc1565610c918a35
Mukhtar B. Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-15 12:01:55 -0700

    main c project file

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Program to print Hello World
+ *
+ * Return: 0 (Success) || 1 (Error)
+ */
+int main(void)
+{
+	printf("Hello world");
+	return (0);
+}
